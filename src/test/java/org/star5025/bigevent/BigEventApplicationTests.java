ea16adae4baa3e331862835a1dc9859a38681283
ZidaWang <starzd5025@gmail.com>
2025-11-01 17:29:23 +0800

    feat: 初始化数据库，新增建表语句

---
+++ src/test/java/org/star5025/bigevent/BigEventApplicationTests.java
@@ -0,0 +1,13 @@
+package org.star5025.bigevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
